cf3f6ce289b7e10cf04e33411a01a6fdae8a8dae
Yogesh More <alexthemed@hotmail.com>
2016-08-19 22:09:14 -0400

    commit

--- server.R
+++ server.R
@@ -2,12 +2,6 @@ library(shiny)
 library(stringr)
 library(ggplot2)
 
-## the following function unordered_table takes a string of text as input 
-## converts it to all lowercase
-## and returns a data frame whose first column is letters of alphabet
-## and second column is number of instances of each letter in mystring
-## and third column is the frequeny (as a percentage) with which each letter appears
-
 unordered_table<-function(mystring)
 { mystring<-tolower(mystring)
   count_vector<-as.integer("")
@@ -20,10 +14,6 @@ unordered_table<-function(mystring)
   df<-data.frame(letters, count_vector, freq)
 }
 
-## the function ordered_table takes a string of text as input
-## calls the underordered_table function on that string
-## then sorts the table based on count of each letter in decreasing frequency
-
 ordered_table<-function(mystring)
 {
   df<-unordered_table(mystring)
@@ -32,9 +22,6 @@ ordered_table<-function(mystring)
   df
 }  
   
-## the percentage_plot function takes a string of text as input
-## call the unordered table function to get the frequency of each letter in the string
-## then makes a scatterplot with the letters on the x-axis and the percentage on y-axis
 percentage_plot<-function(mystring)
 {
   df<-unordered_table(mystring)
--- ui.R
+++ ui.R
@@ -1,6 +1,9 @@
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
   headerPanel("Character frequency in text"),
   sidebarPanel(textInput("id1", "Enter text to analyze", value = "", width = 1000, placeholder = NULL)),
+  sidebarPanel("Documentation: The user enters some text input in the box to the left. The app computes how frequently each letter of the alphabet appears in the user's input, 
+and outputs a table listing how frequently each letter appears. The letters are listed in decreasing order of frequency. 
+               Also, a scatterplot of the frequency per letter is output."),
   mainPanel(
     h3('Output'),
     h4('You entered'),
@@ -10,5 +13,5 @@ shinyUI(pageWithSidebar(
     h4('Percentage plot'),
     plotOutput("percentageplot")
   )
-))
+  ))
 
